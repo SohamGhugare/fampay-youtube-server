01800e066b0c5c9c5616bbcf44274e3f5224500c
Soham Ghugare <soham.ghugare@gmail.com>
2024-03-28 15:12:44 +0530

    fix(model): removed gorm field to fix schema

--- models/video.go
+++ models/video.go
@@ -1,9 +1,6 @@
 package models
 
-import "gorm.io/gorm"
-
 type Video struct {
-	gorm.Model
 	ID           string `json:"id"`
 	Title        string `json:"title"`
 	Description  string `json:"description"`
